2a3a179d838b09cc660825b3c1f6a6c1b075d36b
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2025-01-28 15:02:21 +0000

    Revert "First try at this"
    This reverts commit 2e294875ae744cb330b8585a27b0df10a94ed16d.

--- index.js
+++ index.js
@@ -1,48 +1,27 @@
-require("dotenv").config(".env");
-const cors = require("cors");
-const express = require("express");
+require('dotenv').config('.env');
+const cors = require('cors');
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
+const morgan = require('morgan');
 const { PORT = 3000 } = process.env;
-const { auth } = require("express-openid-connect");
 // TODO - require express-openid-connect and destructure auth from it
 
-const { User, Cupcake } = require("./db");
+const { User, Cupcake } = require('./db');
 
 // middleware
 app.use(cors());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 
 /* *********** YOUR CODE HERE *********** */
 // follow the module instructions: destructure config environment variables from process.env
 // follow the docs:
-// define the config object
-// attach Auth0 OIDC auth router
-// create a GET / route handler that sends back Logged in or Logged out
+  // define the config object
+  // attach Auth0 OIDC auth router
+  // create a GET / route handler that sends back Logged in or Logged out
 
-const { AUTH0_SECRET, AUTH0_AUDIENCE, AUTH0_CLIENT_ID, AUTH0_BASE_URL } =
-  process.env;
-
-const config = {
-  authRequired: true, // this is different from the documentation
-  auth0Logout: true,
-  secret: AUTH0_SECRET,
-  baseURL: AUTH0_AUDIENCE,
-  clientID: AUTH0_CLIENT_ID,
-  issuerBaseURL: AUTH0_BASE_URL,
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
-});
-
-app.get("/cupcakes", async (req, res, next) => {
+app.get('/cupcakes', async (req, res, next) => {
   try {
     const cupcakes = await Cupcake.findAll();
     res.send(cupcakes);
@@ -54,11 +33,12 @@ app.get("/cupcakes", async (req, res, next) => {
 
 // error handling middleware
 app.use((error, req, res, next) => {
-  console.error("SERVER ERROR: ", error);
-  if (res.statusCode < 400) res.status(500);
-  res.send({ error: error.message, name: error.name, message: error.message });
+  console.error('SERVER ERROR: ', error);
+  if(res.statusCode < 400) res.status(500);
+  res.send({error: error.message, name: error.name, message: error.message});
 });
 
 app.listen(PORT, () => {
   console.log(`Cupcakes are ready at http://localhost:${PORT}`);
 });
+
